c658e75ea7305090ff96e339078ff29a6735d98d
Sahal-P <sahlcyprus@gmail.com>
2023-12-14 15:12:26 +0530

    updated: routers.py (commented out test router)

--- src/infrastructure/api/routes/account/routers.py
+++ src/infrastructure/api/routes/account/routers.py
@@ -48,34 +48,34 @@ class UserRouter(SimpleRouter):
     ]
 
 
-class AdminRouter(SimpleRouter):
-    routes = [
-        Route(
-            url=user_router.get_url("user_login"),
-            mapping=user_router.map("user_login"),
-            initkwargs={"viewset_factory": UserViewSetFactory},
-            name="{basename}-login",
-            detail=False,
-        ),
-        Route(
-            url=user_router.get_url("user_register"),
-            mapping=user_router.map("user_register"),
-            initkwargs={"viewset_factory": UserViewSetFactory},
-            name="{basename}-register",
-            detail=False,
-        ),
-        Route(
-            url=user_router.get_url("get_user"),
-            mapping=user_router.map("get_user"),
-            initkwargs={"viewset_factory": UserViewSetFactory},
-            name="{basename}-get_user",
-            detail=False,
-        ),
-        Route(
-            url=user_router.get_url("refresh_token"),
-            mapping=user_router.map("refresh_token"),
-            initkwargs={"viewset_factory": UserViewSetFactory},
-            name="{basename}-refresh_token",
-            detail=False,
-        ),
-    ]
+# class AdminRouter(SimpleRouter):
+#     routes = [
+#         Route(
+#             url=user_router.get_url("user_login"),
+#             mapping=user_router.map("user_login"),
+#             initkwargs={"viewset_factory": UserViewSetFactory},
+#             name="{basename}-login",
+#             detail=False,
+#         ),
+#         Route(
+#             url=user_router.get_url("user_register"),
+#             mapping=user_router.map("user_register"),
+#             initkwargs={"viewset_factory": UserViewSetFactory},
+#             name="{basename}-register",
+#             detail=False,
+#         ),
+#         Route(
+#             url=user_router.get_url("get_user"),
+#             mapping=user_router.map("get_user"),
+#             initkwargs={"viewset_factory": UserViewSetFactory},
+#             name="{basename}-get_user",
+#             detail=False,
+#         ),
+#         Route(
+#             url=user_router.get_url("refresh_token"),
+#             mapping=user_router.map("refresh_token"),
+#             initkwargs={"viewset_factory": UserViewSetFactory},
+#             name="{basename}-refresh_token",
+#             detail=False,
+#         ),
+#     ]
